2b3f72f5eb7c7735f7cb226a9f617b095e0a5b5b
Eaadcom <eaadcom@gmail.com>
2020-06-23 15:05:39 +0200

    initial setup

---
+++ app/src/main/java/com/game/ikpmd/MainActivity.java
@@ -0,0 +1,39 @@
+package com.game.ikpmd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        createListeners();
+    }
+
+    private void createListeners(){
+        Button loginButton = (Button) findViewById(R.id.goToLoginButton);
+        Button registerButton = (Button) findViewById(R.id.goToRegisterButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        registerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
