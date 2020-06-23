1ab134774b4bfc926f50ca2a34b297a25f7c57d9
Eaadcom <eaadcom@gmail.com>
2020-06-23 17:24:28 +0200

    progress in register functionality

--- app/src/main/java/com/game/ikpmd/RegisterActivity.java
+++ app/src/main/java/com/game/ikpmd/RegisterActivity.java
@@ -2,7 +2,11 @@ package com.game.ikpmd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        createListeners();
+    }
+
+    private void createListeners(){
+        Button registerButton = (Button) findViewById(R.id.registerButton);
+
+        registerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+               registerUser();
+            }
+        });
+    }
+
+    private void registerUser(){
+        EditText usernameField = (EditText)findViewById(R.id.registerUsernameField);
+        EditText passwordField = (EditText)findViewById(R.id.registerPasswordField);
+        String username = usernameField.getText().toString();
+        String password = passwordField.getText().toString();
     }
 }
